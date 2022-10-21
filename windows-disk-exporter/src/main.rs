c8d2045654c37919f137781e71b4ae404892bcf7
Rob Scheepens <rob.scheepens@nutanix.com>
2022-10-21 04:41:03 -0700

    Introduced call to PdhAddCounterW().

--- windows-disk-exporter/src/main.rs
+++ windows-disk-exporter/src/main.rs
@@ -8,14 +8,18 @@ fn main() {
     
     unsafe {
 
-    let mut counter_path = PCWSTR::from_raw("Test");
-
+    let counter_path = String::from("Test");
     let mut h_query:isize = 0;
+    let mut h_counter:isize = 0;
     let pdh_status = PdhOpenQueryW(PCWSTR::null(), 0, &mut h_query);
 
     if pdh_status != ERROR_SUCCESS as i32
 	{
 		println!("pdh_status: {}", pdh_status);
 	}
+
+    let pdh_status = PdhAddCounterW(h_query, counter_path, 0, h_counter);
+
+    println!("{}",counter_path);
   }
 }
