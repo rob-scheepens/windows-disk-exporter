0191587f403fa74259506ed7de78af8ac1a99d55
Rob Scheepens <rob.scheepens@nutanix.com>
2022-10-24 07:26:30 -0700

    Debugging pdh_status.

--- windows-disk-exporter/src/main.rs
+++ windows-disk-exporter/src/main.rs
@@ -11,14 +11,16 @@ fn main() {
       let ans = "\\Processor(_Total)\\% Processor Time".as_ptr() as *const u16;
       let counter_path = PCWSTR::from_raw(ans);
       let mut h_query = 0;
-      let h_counter: *mut *mut c_void = 0 as *mut c_void as *mut *mut c_void;
+      let mut h_counter: *mut *mut c_void = 0 as *mut c_void as *mut *mut c_void;
 
-      let pdh_status = PdhOpenQueryW(PCWSTR::null(), 0, &mut h_query);
+      let pdh_status = PdhAddCounterW(h_query as *mut c_void , counter_path.as_ptr(), 0, h_counter);
+
+//      let pdh_status = PdhOpenQueryW(PCWSTR::null(), 0, &mut h_query);
       if pdh_status != ERROR_SUCCESS as i32
       {
         println!("pdh_status: {}", pdh_status);
       }
-      let pdh_status = PdhAddCounterW(h_query as *mut c_void , counter_path.as_ptr(), 0, h_counter);
+
       let pdh_status = PdhCollectQueryData(h_query as *mut c_void);
       println!("{:?}",counter_path);
   }
