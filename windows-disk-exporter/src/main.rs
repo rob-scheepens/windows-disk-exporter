ebd83baca9590e746b8746b1615c796fcd8970a2
Rob Scheepens <rob.scheepens@nutanix.com>
2022-10-25 22:39:23 -0700

    Changed to MSFT support example.

--- windows-disk-exporter/src/main.rs
+++ windows-disk-exporter/src/main.rs
@@ -1,35 +1,28 @@
 use prometheus::{Opts, Registry, Counter, TextEncoder, Encoder};
-use windows::core::*;
-use windows::Win32::System::Performance::PdhOpenQueryW;
-use winapi::ctypes::*;
-use winapi::um::pdh::*;
-use winapi::shared::winerror::*;
 
+use windows::{core::*, Win32::System::Performance::*};
+ 
 fn main() {
-    
     unsafe {
-      let ans = "\\Processor(0)\\% Processor Time".as_ptr() as *const u16;
-      let counter_path = PCWSTR::from_raw(ans);
-      let mut h_query = 0;
-      let h_counter: *mut *mut c_void = 0 as *mut c_void as *mut *mut c_void;
-
-
-      let _pdh_status = PdhOpenQueryW(PCWSTR::null(), 0, &mut h_query);
-
-      if _pdh_status != ERROR_SUCCESS as i32
-      {
-        println!("PdhOpenQueryW - _pdh_status: {}\n", _pdh_status);
-      }
-
-      let _pdh_status = PdhAddCounterW(h_query as *mut c_void , counter_path.as_ptr(), 0, h_counter);
-
-      if _pdh_status != ERROR_SUCCESS as i32
-      {
-        println!("PdhAddCounterW - _pdh_status: {}\n", _pdh_status);
-      }
-
-      let _pdh_status = PdhAddCounterW(h_query as *mut c_void , counter_path.as_ptr(), 0, h_counter);
-      let _pdh_status = PdhCollectQueryData(h_query as *mut c_void);
-      println!("{:?}\n",counter_path);
-  }
+        let mut query = 0;
+        PdhOpenQueryW(None, 0, &mut query);
+ 
+        let mut counter = 0;
+        PdhAddCounterW(
+            query,
+            w!("\\Processor(0)\\% Processor Time"),
+            0,
+            &mut counter,
+        );
+ 
+        loop {
+            std::thread::sleep(std::time::Duration::new(1, 0));
+            PdhCollectQueryData(query);
+ 
+            let mut value = Default::default();
+            if 0 == PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, None, &mut value) {
+                println!("{:.2}", value.Anonymous.doubleValue);
+            }
+        }
+    }
 }
