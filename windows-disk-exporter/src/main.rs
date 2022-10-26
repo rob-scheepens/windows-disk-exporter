d10968cdbfcda945cfd14d8e01d9e6466ffc7d15
Rob Scheepens <rob.scheepens@nutanix.com>
2022-10-26 00:01:41 -0700

    Added physicaldisk sec/read counter.

--- windows-disk-exporter/src/main.rs
+++ windows-disk-exporter/src/main.rs
@@ -7,22 +7,34 @@ fn main() {
         let mut query = 0;
         PdhOpenQueryW(None, 0, &mut query);
  
-        let mut counter = 0;
+        let mut disksecwrite = 0;
         PdhAddCounterW(
             query,
             w!("\\physicaldisk(1)\\avg. disk sec/write"),
             0,
-            &mut counter,
+            &mut disksecwrite
         );
  
+        let mut disksecread = 0;
+        PdhAddCounterW(
+            query,
+            w!("\\physicaldisk(1)\\avg. disk sec/read"),
+            0,
+            &mut disksecread
+        );
+
         loop {
             std::thread::sleep(std::time::Duration::new(1, 0));
             PdhCollectQueryData(query);
  
-            let mut value = Default::default();
-            if 0 == PdhGetFormattedCounterValue(counter, PDH_FMT_DOUBLE, None, &mut value) {
-                println!("{:.4}", value.Anonymous.doubleValue * 1000.0);
-            }
+            let mut value_write = Default::default();
+            if 0 == PdhGetFormattedCounterValue(disksecwrite, PDH_FMT_DOUBLE, None, &mut value_write) {
+	    
+	    let mut value_read = Default::default();
+	    if 0 == PdhGetFormattedCounterValue(disksecread, PDH_FMT_DOUBLE, None, &mut value_read) {
+
+                println!("{:.4}\t  {:.4}", value_write.Anonymous.doubleValue * 1000.0, value_read.Anonymous.doubleValue * 1000.0);
+            }}
         }
     }
 }
